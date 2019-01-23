0ef4ee4dded28cd86c2705c46ff26243f3493411
ARRAY-LIST <1348909544@qq.com>
2019-01-24 00:33:21 +0800

    v-head...

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
+    <v-header></v-header>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+  import VHeader from '@/components/v-header/v-header'
 
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app',
+    components: {
+      VHeader
+    }
+  }
 </script>
 <style lang="stylus">
   #app
---
+++ src/components/v-header/v-header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <!--avatar and a little info block-->
+    <div class="content-wrapper">
+    </div>
+    <!--bulletin block-->
+    <div class="bulletin-wrapper">
+    </div>
+    <!--blur background block-->
+    <div class="background">
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'v-header'
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "../../common/stylus/mixin"
+  @import "../../common/stylus/variable"
+</style>
