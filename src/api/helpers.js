0c8bd072c65907a4ac1d3a4dce612c4fee93956d
ARRAY-LIST <1348909544@qq.com>
2019-02-24 20:31:15 +0800

    done -  url

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,10 +1,15 @@
 import axios from 'axios'
 
+const urlMap = {
+  development: '/',
+  production: 'http://superdev.top/sell/'
+}
+const publicPath = urlMap[process.env.NODE_ENV]
 const ERR_OK = 0
 
 export function get (url) {
   return function (params) {
-    return axios.get(url, {
+    return axios.get(publicPath + url, {
       params
     }).then((res) => {
       const { errno, data } = res.data
